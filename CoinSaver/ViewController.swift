1d645f3bd5b7dd360ce8c7a8c5281eb710f11828
Ryan Morrison <ryan.morrison@bbc.co.uk>
2018-08-12 12:02:30 +0100

    Initial Commit

---
+++ CoinSaver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoinSaver
+//
+//  Created by Ryan Morrison on 12/08/2018.
+//  Copyright © 2018 Ryan Morrison. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
